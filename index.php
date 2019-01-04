5ac7e2a04d2e66427dd58007fe66918e8829913a
Rafał Daniś <r.danis88@gmail.com>
2019-01-04 22:51:02 +0100

    master

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+//$fb = new Facebook\Facebook();
+//$unit = new ;
+
+//print_r($fb);
+
+final class EmailTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(true);
+    }
+}
+
+//./vendor/bin/phpunit --bootstrap vendor/autoload.php index
